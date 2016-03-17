14a73cefa4f72b92ad6c633543ebb809c8823ff3
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-03-17 22:54:30 +0100

    Fix quotes, number of arguments in deploy script

--- etc/deploy/stages/staging.rb
+++ etc/deploy/stages/staging.rb
@@ -1,9 +1,9 @@
 server "staging.sulu.io", user: "sulu", roles: [:app, :web]
 
 after "deploy:updated", :build do
-    invoke 'symfony:console', 'sulu:build', 'prod', '--no-interaction'
-    invoke 'symfony:console', 'sulu:translate:import', 'en'
-    invoke 'symfony:console', 'sulu:translate:import', 'de'
-    invoke 'symfony:console', 'sulu:translate:export', 'en'
-    invoke 'symfony:console', 'sulu:translate:export', 'de'
+    invoke "symfony:console", "sulu:build", "prod --no-interaction"
+    invoke "symfony:console", "sulu:translate:import", "en"
+    invoke "symfony:console", "sulu:translate:import", "de"
+    invoke "symfony:console", "sulu:translate:export", "en"
+    invoke "symfony:console", "sulu:translate:export", "de"
 end
