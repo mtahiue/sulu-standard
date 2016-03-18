b6e6e1af31f7d6c4ae5cac0e77c2ea4480e62f29
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-03-18 18:33:03 +0100

    Add French translations to deployment script

--- etc/deploy/stages/staging.rb
+++ etc/deploy/stages/staging.rb
@@ -4,6 +4,8 @@ after "deploy:updated", :build do
     invoke "symfony:console", "sulu:build", "prod --no-interaction"
     invoke "symfony:console", "sulu:translate:import", "en"
     invoke "symfony:console", "sulu:translate:import", "de"
+    invoke "symfony:console", "sulu:translate:import", "fr"
     invoke "symfony:console", "sulu:translate:export", "en"
     invoke "symfony:console", "sulu:translate:export", "de"
+    invoke "symfony:console", "sulu:translate:export", "fr"
 end
